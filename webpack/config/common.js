f3f89d90771e4fd7d4157e2ac9cd3a772060a655
Rhoulhac <greg.rhoulhac@gmail.com>
2018-07-18 20:33:04 -0500

    Updating webpack config

--- webpack/config/common.js
+++ webpack/config/common.js
@@ -1,8 +1,10 @@
 // Created by kirby15 on 2/1/18.
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const autoprefixer = require('autoprefixer');
 
 const isProduction = process.env.NODE_ENV === 'production';
 const scssCommonLoaders = [
+    'style-loader',
     {
         loader: 'css-loader',
         options: {
@@ -13,15 +15,14 @@ const scssCommonLoaders = [
     {
         loader: 'sass-loader',
         options: { sourceMap: !isProduction }
+    },
+    {
+        loader: 'postcss-loader',
+        options: {
+            plugins: [autoprefixer({ browsers: 'last 2 versions' })]
+        }
     }
 ];
-
-if (isProduction) {
-    scssCommonLoaders.splice(0, 0, MiniCssExtractPlugin.loader);
-    scssCommonLoaders.splice(2, 0, 'postcss-loader');
-} else {
-    scssCommonLoaders.splice(0, 0, 'style-loader');
-}
 
 module.exports = {
     module: {
